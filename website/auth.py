6464d6ba16c153f9aa0e85bf40b98ec90f6cfdd6
QiongzhuLi <lqiongzhu@126.com>
2022-12-21 17:52:17 +0800

    resquest ways, GET & POST, and set up flash

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
     return render_template("login.html", text='hello there',boolean='f', user=current_user)
 
@@ -12,8 +12,26 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('firstName must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('password must be greater than 6 characters.', category='error')            
+        else:
+            # add user to the database
+            flash('Account created', category='success')
+            
     return render_template("sign_up.html", user=current_user)
 
 
