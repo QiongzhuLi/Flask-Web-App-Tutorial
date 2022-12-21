1e711c85743a14ce5b7a32ea37ffbbfa467c33d9
QiongzhuLi <lqiongzhu@126.com>
2022-12-22 00:27:54 +0800

    learn decorator @login_required

--- website/views.py
+++ website/views.py
@@ -4,5 +4,6 @@ from flask_login import login_user, login_required, logout_user, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/')   ##It is a decorator, which means whenever you go to this route, the following function will run
+@login_required   ## login required, see __init__.py
 def home():
     return render_template("home.html", user=current_user)
