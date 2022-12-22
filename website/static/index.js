cecc4d646ef538b2a27367f78f22e8e020879d87
QiongzhuLi <lqiongzhu@126.com>
2022-12-22 15:05:26 +0800

    Use Javascripts to deleteNote, will be used in home.html

--- website/static/index.js
+++ website/static/index.js
@@ -1,4 +1,4 @@
-function deleteNote(noteId) {
+function deleteNote(noteId) {  //Use Javascripts to deleteNote, will be used in home.html
     fetch("/delete-note", {
       method: "POST",
       body: JSON.stringify({ noteId: noteId }),
